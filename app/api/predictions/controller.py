dfb18083778dbbeee88fb8b6ebcb1938ea7a031e
NUicich <nicolasu@brainstems.ai>
2024-08-06 19:10:22 -0300

    add end of response token, api key auth

--- app/api/predictions/controller.py
+++ app/api/predictions/controller.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter, HTTPException
+import json
+import os
+from fastapi import APIRouter, HTTPException, Request, Depends
 from pydantic import BaseModel
+
+from app.utils import check_api_key
 from .service import PredictionService
 
 router = APIRouter()
@@ -10,15 +14,31 @@ class PredictionRequest(BaseModel):
     prediction: str
     address: str
 
+async def get_api_key(request: Request):
+    api_key = request.headers.get("api_key_auth")
+    check_api_key(api_key)
+    return api_key
+
 @router.post("/", response_model=dict)
-async def create_prediction(request: PredictionRequest):
-    prediction_service = PredictionService()
+async def create_prediction(request: PredictionRequest, api_key: str = Depends(get_api_key)):
     try:
         result = prediction_service.save_prediction(request.prediction, request.address)
-        return result
+        return {"result": result}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/new", response_model=dict)
-async def get_new_prediction():
-    PredictionService.get_new_prediction()
+async def get_new_prediction(api_key: str = Depends(get_api_key)):
+    try:
+        result = await  prediction_service.get_new_prediction()
+        return {"result": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/daily", response_model=dict)
+async def get_daily_event(api_key: str = Depends(get_api_key)):
+    try:
+        result = await prediction_service.get_daily_event()
+        return {"result": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- app/api/predictions/service.py
+++ app/api/predictions/service.py
@@ -12,6 +12,7 @@ class PredictionService:
     def __init__(self):
         self.dynamodb = boto3.resource('dynamodb', region_name=os.environ.get("AWS_REGION"))
         self.table = self.dynamodb.Table('bs-olympics-results')
+        self.events = self.dynamodb.Table('bs-olympics-context-prompts')
 
     def save_prediction(self, prediction: str, address: str):
         timestamp = datetime.now(timezone.utc).isoformat()
@@ -34,6 +35,10 @@ class PredictionService:
     @classmethod
     async def get_new_prediction(cls, prompt: str, client_websocket):
         prompt_key = 'MARATHON_SWIMMING_MEN'
+        # prompt_key = await cls.get_daily_event()
+        if not prompt_key:
+            await client_websocket.send_text(json.dumps({'statusCode': 404, 'body': 'No daily event found'}))
+            return
         prompts = get_prompts_from_dynamodb(prompt_key)
         system_context_prompt = prompts['contextPrompt']
         assistant_context_prompt = prompts['assistantPrompt']
@@ -49,10 +54,27 @@ class PredictionService:
                         async for message in ws:
                             print('Received message from external websocket:', message)
                             await client_websocket.send_text(json.dumps({"token": message}))
+                        await client_websocket.send_text(json.dumps({"token": 'END_OF_RESPONSE'}))
                     except Exception as e:
                         print("Error receiving message via WebSocket:", e)
                     break
             except Exception as e:
                 print(f"Unable to connect to Inference Server. Retrying in {os.environ.get('RETRY_TIME')} seconds:", e)
-                await asyncio.sleep(os.get('RETRY_TIME')) 
-            
+                await asyncio.sleep(os.environ.get('RETRY_TIME')) 
+
+    @classmethod
+    async def get_daily_event(cls):
+        try:
+            current_day = datetime.now().strftime('%A').upper()
+            response = cls().events.query(
+                IndexName='day-index',
+                KeyConditionExpression=boto3.dynamodb.conditions.Key('day').eq(current_day)
+            )
+            items = response.get('Items', [])
+            if items:
+                daily_event = items[0].get('sport_key', '')
+                return daily_event
+            else:
+                return None
+        except ClientError as e:
+            raise Exception(e.response['Error']['Message'])
