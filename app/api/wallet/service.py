77659d1d35a8722ab7c92e32ced4ec1411542226
NUicich <nicolasu@brainstems.ai>
2024-08-14 10:01:07 -0300

    add automation for each event

--- app/api/wallet/service.py
+++ app/api/wallet/service.py
@@ -5,8 +5,8 @@ from botocore.exceptions import ClientError
 class WalletService:
     def __init__(self):
         self.dynamodb = boto3.resource('dynamodb', region_name=os.environ.get("AWS_REGION"))
-        self.wallets = self.dynamodb.Table('bs-olympics-whitelisted-addresses')
-        
+        self.wallets = self.dynamodb.Table('bs-user-contacts')
+
 
     def get_wallets(self):
         response = self.wallets.scan()
@@ -15,11 +15,14 @@ class WalletService:
 
     def get_wallet_by_address(self, address: str):
         try:
-            response = self.wallets.get_item(Key={'address': address})
-            wallet = response.get('Item')
-            if not wallet:
+            response = self.wallets.query(
+                IndexName='address-index',
+                KeyConditionExpression=boto3.dynamodb.conditions.Key('address').eq(address)
+            )
+            items = response.get('Items')
+            if not items:
                 raise Exception("Wallet not found")
-            return wallet
+            return items[0] 
         except ClientError as e:
             raise Exception(e.response['Error']['Message'])
 
